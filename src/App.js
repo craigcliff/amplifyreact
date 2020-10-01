646c1ae0c4a838464e7d810c8dc26182f4efc7e6
craigcliff <craigcliff@gmail.com>
2020-10-01 09:09:38 +0200

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from App version 2</h1>
-      </header>
-    </div>
-  );
+      <main>
+        <Navbar />
+          <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={Error} />
+          </Switch>
+      </main>
+  )
 }
 
 export default App;
